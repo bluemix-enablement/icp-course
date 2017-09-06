2fa9dc7d1a757974289fcc3e09a1fa84aedc3da4
vbudi000 <vbudi@yahoo.com>
2017-09-06 12:20:30 -0500

    update resetcert

--- resetcert.sh
+++ resetcert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-  sudo cp oidc.crt /usr/local/share/ca-certificates/oidc.crt
+  sudo scp root@master:/etc/cfc/conf/oidc.crt /usr/local/share/ca-certificates/oidc.crt
   sudo update-ca-certificates
   sudo service docker restart
 exit
