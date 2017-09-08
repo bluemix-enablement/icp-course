762406709e23f07e547af15e633ba5d72607725c
vbudi000 <vbudi@yahoo.com>
2017-09-08 13:40:13 -0500

    add variable master

--- icplogin.sh
+++ icplogin.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  master="master"
+else
+  master=$1
+fi
+
 # Then login using kubectl
   token=""
   while [ "$token" = "" ]
   do
-    token=`curl -k -s -d '{"uid":"admin","password":"admin"}' http://master:8101/acs/api/v1/auth/login | grep -Po '(?<="token":)(.*?)(?=})' | sed 's/\(^"\|"$\)//g'`
+    token=`curl -k -s -d '{"uid":"admin","password":"admin"}' http://${master}:8101/acs/api/v1/auth/login | grep -Po '(?<="token":)(.*?)(?=})' | sed 's/\(^"\|"$\)//g'`
      if [ "$token" = "" ]
      then
        printf "."
@@ -12,7 +18,7 @@
      fi
   done
 
-  kubectl config set-cluster cfc --server=https://master:8001 --insecure-skip-tls-verify=true
+  kubectl config set-cluster cfc --server=https://${master}:8001 --insecure-skip-tls-verify=true
   kubectl config set-credentials user --token=$token
   kubectl config set-context cfc --cluster=cfc
   kubectl config set-context cfc --user=user --namespace=default
